d5e5c9de79a820d4d72d2792112d681445c84b88
DimaSuvorin <dmsuvorin@gmail.com>
2022-05-08 14:41:33 +0300

    first version

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
 fun main() {
     println("Hello Kotlin!")
+    val amount = 100_000_00 //сумма перевода
+    val commission = 0.0075 //комиссия 0,75% от суммы перевода
+    val commissionMin = 35_00 //минимальная сумма комиссии 35 рублей
+
+    val totalCommission = amount * commission
+    val result = if (totalCommission <= commissionMin) commissionMin else totalCommission
+
+    println("Total commission: $result")
 }
